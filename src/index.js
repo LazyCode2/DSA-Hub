cde97b94f2dc4cd22dbd38419ad319657bc87914
Lazy_Developer <163510211+LazyCode2@users.noreply.github.com>
2025-07-06 17:22:25 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const helmet = require('helmet');
+const serverless = require('serverless-http');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-// Configuring CSP 
+// Apply Helmet
 app.use(
   helmet({
     contentSecurityPolicy: {
@@ -15,7 +15,7 @@ app.use(
           "'self'",
           "https://cdn.tailwindcss.com",
           "'sha256-Ua7xizGnAIIEu3oDw8Y8hRyF3ks2JpUzYZVgy828eFk='",
-          "'sha256-SIDHjR1vwyncuRi5m71SeGPWO3Vv+9OjzLq+OWor71I='"
+          "'sha256-SIDHjR1vwyncuRi5m71SeGPWO3Vv+9OjzLq+OWor71I='",
         ],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
         fontSrc: ["'self'", "https://fonts.gstatic.com"],
@@ -26,19 +26,23 @@ app.use(
   })
 );
 
-// Serve favicon first so Helmet runs
+// Serve favicon first
 app.get('/favicon.ico', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'favicon.ico'));
+  res.sendFile(path.join(__dirname, '../public', 'favicon.ico'));
 });
 
-// Then static files
-app.use(express.static(path.join(__dirname, 'public')));
+// Static files
+app.use(express.static(path.join(__dirname, '../public')));
 
-// Then other routes
+// Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.get('/learn', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'chapter.html'));
+  res.sendFile(path.join(__dirname, '../public', 'chapter.html'));
 });
+
+// Vercel will call this handler instead
+module.exports = app;
+module.exports.handler = serverless(app);
